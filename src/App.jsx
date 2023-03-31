456b8f872cb4a400a6320bc704033f77d25a17d5
favour orji <favour@learnflo.co>
2023-03-31 20:06:50 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experince/Ex'
+import Services from './components/nav/Nav'
+import Portfolio from './components/nav/Nav'
+import Testimonials from './components/nav/Nav'
+import Contact from './components/nav/Nav'
+import Footer from './components/nav/Nav'
 const App = () => {
   return (
     <>
       <Header />
       <Nav/>
+      <About/>
+      <Experience/>
+      <Services/>
+      <Portfolio/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
     </>
+
   )
 }
 
